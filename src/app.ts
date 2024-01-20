fd5633dd868326cd078bdd8a69039849e1a166d8
syedalamin <syedalamin2580@gmail.com>
2024-01-20 18:30:03 +0600

    UM-7

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors'
 import express, { Application, Request, Response } from 'express'
+import userRoutes from './app/modules/users/users.route'
 const app: Application = express()
 
 app.use(cors())
@@ -8,8 +9,11 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// Application route
+app.use('/api/v1/users', userRoutes)
+
 //testing
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
   res.send('Working successfully 🙂 ')
 })
 
---
+++ src/app/modules/users/user.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express'
+import usersService from './users.service'
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const { user } = req.body
+    const result = await usersService.createUser(user)
+    res.status(200).json({
+      success: true,
+      message: 'User created Successfully!',
+      data: result,
+    })
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: 'Fail to create user',
+    })
+  }
+}
+
+export default { createUser }
